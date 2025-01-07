4446cccd98f502b2840d8bc9f7bbe62b862439a2
RofiNugraha <rofinugraha549@gmail.com>
2025-01-07 16:31:59 +0700

    menambahkan read dan route layanan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\AdminLayananController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,6 +22,14 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', \App\Http\Middleware\AdminMiddleware::class])->group(function () {
     Route::get('/admin/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/layanan', [AdminLayananController::class, 'index'])->name('admin.layanan.index');
+    Route::get('layanan/create', [AdminLayananController::class, 'create'])->name('admin.layanan.create');
+    Route::post('layanan', [AdminLayananController::class, 'store'])->name('admin.layanan.store');
+    Route::get('layanan/{layanan}', [AdminLayananController::class, 'show'])->name('admin.layanan.show');
+    Route::get('layanan/{layanan}/edit', [AdminLayananController::class, 'edit'])->name('admin.layanan.edit');
+    Route::put('layanan/{layanan}', [AdminLayananController::class, 'update'])->name('admin.layanan.update');
+    Route::delete('layanan/{layanan}', [AdminLayananController::class, 'destroy'])->name('admin.layanan.destroy');
 });
 
 require __DIR__.'/auth.php';
