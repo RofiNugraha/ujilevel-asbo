22e40e2f21e1dfb038ba27efc6aa2d6cebdfc9fc
RofiNugraha <rofinugraha549@gmail.com>
2025-01-30 11:21:35 +0700

    revisi checkout

--- database/migrations/2025_01_30_041915_add_gambar_to_produks_table.php
+++ database/migrations/2025_01_30_041915_add_gambar_to_produks_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jenis_layanans', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama');
-            $table->string('deskripsi')->nullable();
-            $table->decimal('harga', 10, 2);
-            $table->timestamps();
+        Schema::table('produks', function (Blueprint $table) {
+            $table->string('gambar')->nullable();
         });
     }
 
@@ -25,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jenis_layanans');
+        Schema::table('produks', function (Blueprint $table) {
+            $table->dropColumn('gambar');
+        });
     }
 };
