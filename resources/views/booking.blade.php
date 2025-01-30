328fa8c6a2a0c9667a8e35a198ad06f8e5d433f2
RofiNugraha <rofinugraha549@gmail.com>
2025-01-30 11:04:37 +0700

    menambah tabel produk dan jenis layanan

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -46,12 +46,12 @@
             <div class="bg-white rounded-3xl shadow-lg p-6 w-60 h-86 flex flex-col items-center">
                 <img src="{{ asset('images/book.jpg') }}" alt="Service Image"
                     class="rounded-3xl mb-4 w-40 h-40 object-cover">
-                <h2 class="text-2xl font-semibold mb-2 text-center">Lorem, ipsum.</h2>
+                <h2 class="text-2xl font-semibold mb-2 text-center">ea</h2>
                 <h5 class="font-semibold mb-2 text-center">Rp. 15.000,00</h5>
                 @auth
                 <button
                     class="bg-blue-600 mt-4 mb-2 text-white rounded-full px-6 py-2 shadow-md transform hover:translate-y-1 hover:shadow-lg transition-all duration-300">
-                    <a href="{{ route('form') }}">Book Now</a>
+                    <a href="">Book Now</a>
                 </button>
                 @else
                 <button
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\AdminLayananController;
 use App\Http\Controllers\Admin\AdminNotifikasiController;
 use App\Http\Controllers\CekLoginController;
 use App\Http\Controllers\Admin\AdminRiwayatTransaksiController;
+use App\Http\Controllers\LayananController;
 use App\Http\Controllers\UpdateProfileController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -70,12 +71,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::middleware(['auth'])->group(function () {
 
-    // formbook
-    Route::get('/formbook', [CekLoginController::class, 'form'])->name('form');
-    Route::post('/formbook', [CekLoginController::class, 'store'])->name('formbook.store');
-
-    // overview
-    Route::get('/overview', [AdminBookingController::class, 'showTodayBookings'])->name('overview');
+    //input layanan
+    Route::post('/', [LayananController::class, 'store'])->name('layanan.store');
 
     // profil
     Route::get('/profil', [ProfileController::class, 'show'])->name('profil');
@@ -95,33 +92,6 @@ Route::middleware(['auth', \App\Http\Middleware\AdminMiddleware::class])->group(
     Route::get('/admin/layanan/{id}/edit', [AdminLayananController::class, 'edit'])->name('admin.layanan.edit');
     Route::put('/admin/layanan/{id}', [AdminLayananController::class, 'update'])->name('admin.layanan.update');
     Route::delete('/admin/layanan/{layanan}', [AdminLayananController::class, 'destroy'])->name('admin.layanan.destroy');
-
-    // CRUD admin booking
-    Route::get('/admin/booking', [AdminBookingController::class, 'index'])->name('admin.booking.index');
-    Route::get('/admin/booking/create', [AdminBookingController::class, 'create'])->name('admin.booking.create');
-    Route::post('/admin/booking', [AdminBookingController::class, 'store'])->name('admin.booking.store');
-    Route::get('/admin/booking/{id}', [AdminBookingController::class, 'show'])->name('admin.booking.show');
-    Route::get('/admin/booking/{id}/edit', [AdminBookingController::class, 'edit'])->name('admin.booking.edit');
-    Route::put('/admin/booking/{id}', [AdminBookingController::class, 'update'])->name('admin.booking.update');
-    Route::delete('/admin/booking/{id}', [AdminBookingController::class, 'destroy'])->name('admin.booking.destroy');
-    
-    // CRUD admin notifikasi
-    Route::get('/admin/notifikasi', [AdminNotifikasiController::class, 'index'])->name('admin.notifikasi.index');
-    Route::get('/admin/notifikasi/create', [AdminNotifikasiController::class, 'create'])->name('admin.notifikasi.create');
-    Route::post('/admin/notifikasi', [AdminNotifikasiController::class, 'store'])->name('admin.notifikasi.store');
-    Route::get('/admin/notifikasi/{id}', [AdminNotifikasiController::class, 'show'])->name('admin.notifikasi.show');
-    Route::get('/admin/notifikasi/{id}/edit', [AdminNotifikasiController::class, 'edit'])->name('admin.notifikasi.edit');
-    Route::put('/admin/notifikasi/{id}', [AdminNotifikasiController::class, 'update'])->name('admin.notifikasi.update');
-    Route::delete('/admin/notifikasi/{id}', [AdminNotifikasiController::class, 'destroy'])->name('admin.notifikasi.destroy');
-    
-    // CRUD admin riwayat transaksi
-    Route::get('/admin/riwayat', [AdminRiwayatTransaksiController::class, 'index'])->name('admin.riwayat.index');
-    Route::get('/admin/riwayat/create', [AdminRiwayatTransaksiController::class, 'create'])->name('admin.riwayat.create');
-    Route::post('/admin/riwayat', [AdminRiwayatTransaksiController::class, 'store'])->name('admin.riwayat.store');
-    Route::get('/admin/riwayat/{id}', [AdminRiwayatTransaksiController::class, 'show'])->name('admin.riwayat.show');
-    Route::get('/admin/riwayat/{id}/edit', [AdminRiwayatTransaksiController::class, 'edit'])->name('admin.riwayat.edit');
-    Route::put('/admin/riwayat/{id}', [AdminRiwayatTransaksiController::class, 'update'])->name('admin.riwayat.update');
-    Route::delete('/admin/riwayat/{id}', [AdminRiwayatTransaksiController::class, 'destroy'])->name('admin.riwayat.destroy');
     
     // CRUD admin kasir
     Route::get('/admin/kasir', [AdminKasirController::class, 'index'])->name('admin.kasir.index');
